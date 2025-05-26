898134a2acb03b40f0e35322952dfcf15f566b08
Frederick O'Brien <frederik.obrien@gmail.com>
2025-05-26 12:07:07 +0100

    Groundwork

---
+++ app/src/lib/components/layout/ButtonWrapper.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let { children, onClickFunction }: { children: Snippet; onClickFunction: () => void } = $props();
+</script>
+
+<div
+	onclick={onClickFunction}
+	onkeydown={(e) => {
+		if (e.key === 'Enter' || e.key === ' ') {
+			onClickFunction;
+		}
+	}}
+	role="button"
+	tabindex="0"
+>
+	{@render children()}
+</div>
--- app/src/lib/components/layout/Header.svelte
+++ app/src/lib/components/layout/Header.svelte
@@ -1,25 +1,40 @@
-<script>
-	let { userIsLoggedIn } = $props();
+<script lang="ts">
+	import Menu from '../icons/Menu.svelte';
+	import Search from '../icons/Search.svelte';
+	import ButtonWrapper from './ButtonWrapper.svelte';
+
+	let menuIsOpen = $state(false);
+	let searchIsOpen = $state(false);
 </script>
 
 <header>
-	<div class="section"></div>
-	<div class="section">
-		<a href="/" class="logo">
-			<img src="/full-logo-white.png" class="icon dark" alt="Soli emblem" />
-			<img src="/full-logo-black.png" class="icon light" alt="Soli emblem" />
-		</a>
-	</div>
-	<div class="section">
-		<div>
-			{#if userIsLoggedIn}
-				<a href="/account">Account</a>
-			{:else}
-				<a href="/login">Log in</a>
-			{/if}
-		</div>
-	</div>
+	<ButtonWrapper onClickFunction={() => (searchIsOpen = !searchIsOpen)}>
+		<Search />
+	</ButtonWrapper>
+	<a href="/">
+		<img src="/full-logo-white.png" class="icon dark" alt="Soli emblem" />
+		<img src="/full-logo-black.png" class="icon light" alt="Soli emblem" />
+	</a>
+	<ButtonWrapper onClickFunction={() => (menuIsOpen = !menuIsOpen)}>
+		<Menu />
+	</ButtonWrapper>
 </header>
+
+{#if menuIsOpen}
+	<nav class="menu">
+		<ul>
+			<li><a href="/about">Playlists</a></li>
+			<li><a href="/">Collections</a></li>
+			<li><a href="/account">Account</a></li>
+		</ul>
+	</nav>
+{/if}
+
+{#if searchIsOpen}
+	<div class="search-container">
+		<input type="text" placeholder="Search..." />
+	</div>
+{/if}
 
 <style>
 	header {
@@ -27,26 +42,26 @@
 		justify-content: space-between;
 		align-items: center;
 		margin: 1rem;
+		max-height: 60px;
 		a {
 			text-decoration: none;
 			color: inherit;
 		}
 	}
-	.section {
-		flex: 1;
-		display: flex;
-		justify-content: center;
-	}
-
-	.section:first-child > div {
-		margin-right: auto;
-	}
-
-	.section:last-child > div {
-		margin-left: auto;
-	}
 	.icon {
 		height: 50px;
+	}
+	input {
+		width: 90%;
+		max-width: 400px;
+		padding: 0.5rem;
+		margin: 0 1rem;
+		border: 1px solid var(--color-accent);
+		border-radius: 4px;
+		font-size: 1rem;
+	}
+	.search-container {
+		width: 100%;
 	}
 	@media (min-width: 600px) {
 		.icon:hover {
