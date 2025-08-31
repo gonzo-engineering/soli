0d9a5ac6ebb34038c464ae0e4f6cdc1de5487459
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 22:35:28 +0100

    Update hooks.server.ts

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -13,6 +13,7 @@ export const handle: Handle = async ({ resolve, event }) => {
 
 	const response = await resolve(event);
 	response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	response.headers.append('Access-Control-Allow-Headers', '*');
 	response.headers.append('Access-Control-Allow-Origin', origin ? origin : '*');
 	return response;
 };
