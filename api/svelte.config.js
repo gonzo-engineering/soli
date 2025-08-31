d48b91e580b75b825d5e2c9c0244903ad4f33c7c
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 18:50:19 +0100

    Update svelte.config.js

--- api/svelte.config.js
+++ api/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { APP_DOMAIN, DASHBOARD_DOMAIN } from '../shared/config'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		csrf: {
-			checkOrigin: false
+			checkOrigin: false,
+			trustedOrigins: [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com']
 		}
 	}
 };
