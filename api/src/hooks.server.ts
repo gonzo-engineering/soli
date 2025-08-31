3ea67d0b367aed39bd5d35c2f5fe40f0a964901c
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 19:32:38 +0100

    Update hooks.server.ts

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -1,17 +1,8 @@
 import { dev } from '$app/environment';
-import type { Handle, HandleFetch } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 import { APP_DOMAIN, DASHBOARD_DOMAIN } from '../../shared/config';
 
 const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com'];
-
-export const handleFetch: HandleFetch = async ({ request, fetch }) => {
-	const origin = request.headers.get('origin');
-
-	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
-		return new Response('Unauthorized', { status: 401 });
-	}
-	return fetch(request);
-};
 
 export const handle: Handle = async ({ resolve, event }) => {
 	const origin = event.request.headers.get('origin');
