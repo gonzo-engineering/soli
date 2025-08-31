d90c6c85da5e5bcfa7132ae105115d34bb78e89d
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 20:15:35 +0100

    Sinking

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -7,16 +7,12 @@ const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.
 export const handle: Handle = async ({ resolve, event }) => {
 	const origin = event.request.headers.get('origin');
 
-	if (!origin) {
-		return new Response('Unauthorized', { status: 401 });
-	}
-
-	if (!domainsWithAccessToAPI.includes(origin) && !dev) {
+	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
 		return new Response('Unauthorized', { status: 401 });
 	}
 
 	const response = await resolve(event);
 	response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-	response.headers.append('Access-Control-Allow-Origin', origin);
+	response.headers.append('Access-Control-Allow-Origin', origin ? origin : '*');
 	return response;
 };
