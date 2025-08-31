60c118ea7ac16588672a3ea56e13e0d09f099fda
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 19:23:22 +0100

    Update hooks.server.ts
    https://snippets.khromov.se/configure-cors-in-sveltekit-to-access-your-api-routes-from-a-different-host/

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -1,14 +1,30 @@
 import { dev } from '$app/environment';
-import type { HandleFetch } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { APP_DOMAIN, DASHBOARD_DOMAIN } from '../../shared/config';
+
+const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com'];
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
 	const origin = request.headers.get('origin');
-
-	const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com'];
 
 	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
 		return new Response('Unauthorized', { status: 401 });
 	}
 	return fetch(request);
+};
+
+export const handle: Handle = async ({ resolve, event }) => {
+	const origin = event.request.headers.get('origin');
+
+	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+
+	if (!origin) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+
+	const response = await resolve(event);
+	response.headers.append('Access-Control-Allow-Origin', origin);
+	return response;
 };
