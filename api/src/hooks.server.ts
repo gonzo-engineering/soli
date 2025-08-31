ce2c073eda716bed951dc0f46218bf9d7b215055
Frederick O'Brien <frederik.obrien@gmail.com>
2025-08-31 18:21:49 +0100

    Add dashboard to permitted origins

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -1,13 +1,13 @@
 import { dev } from '$app/environment';
 import type { HandleFetch } from '@sveltejs/kit';
-import { APP_DOMAIN } from '../../shared/config';
+import { APP_DOMAIN, DASHBOARD_DOMAIN } from '../../shared/config';
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
 	const origin = request.headers.get('origin');
 
-	const allowedDomains = [APP_DOMAIN, 'https://checkout.stripe.com'];
+	const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com'];
 
-	if ((origin === null || !allowedDomains.includes(origin)) && !dev) {
+	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
 		return new Response('Unauthorized', { status: 401 });
 	}
 	return fetch(request);
