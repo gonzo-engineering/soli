290b3f2e001889d58f0d6ac67952eae23189beca
Frederick O'Brien <frederik.obrien@gmail.com>
2025-09-02 00:51:49 +0100

    Update hooks.server.ts

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 import { APP_DOMAIN, DASHBOARD_DOMAIN } from '../../shared/config';
 
 const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.stripe.com'];
-const publicEndpoints = ['/checkout/success'];
+const publicEndpoints = ['/', '/checkout/success'];
 
 const appendHeaders = (response: Response, origin: string | null) => {
 	response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -23,7 +23,7 @@ export const handle: Handle = async ({ resolve, event }) => {
 
 	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
 		console.error(`Unauthorized access attempt from origin: ${origin}`);
-		return new Response(`Domain ${origin} is not allowed to access this API`, { status: 401 });
+		return new Response(`Domain '${origin}' is not allowed to access this API`, { status: 401 });
 	}
 
 	const response = await resolve(event);
