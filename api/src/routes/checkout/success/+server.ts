740b81295e2dc3e30dbf4f35774bb45c09b9d30f
Frederick O'Brien <frederik.obrien@gmail.com>
2025-09-01 21:22:17 +0100

    Update +server.ts

--- api/src/routes/checkout/success/+server.ts
+++ api/src/routes/checkout/success/+server.ts
@@ -1,4 +1,5 @@
-import { REVENUE_SPLIT } from '../../../../../shared/config';
+import { supabase } from '$lib/server/supabase';
+import { REVENUE_SPLIT, TABLES } from '../../../../../shared/config';
 import { type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
@@ -29,18 +30,19 @@ export const POST: RequestHandler = async ({ request }) => {
 	const topUpAmount: number = stripeSession.amount_total;
 	const topUpTokens = Math.round(topUpAmount * REVENUE_SPLIT.artists);
 
-	const response = await fetch(`/users/${userId}`, {
-		method: 'PATCH',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ balanceChange: topUpTokens })
-	});
+	const { error } = await supabase
+		.from(TABLES.users)
+		.update({
+			tokens_balance: stripeSession.metadata.balance + topUpTokens,
+			updated_at: new Date()
+		})
+		.eq('id', userId)
+		.select(`first_name, tokens_balance, pay_per_stream`)
+		.single();
 
-	if (!response.ok) {
-		const errorBody = await response.json().catch(() => ({}));
-		console.error('Error updating user balance:', errorBody);
-		return new Response(JSON.stringify({ message: 'Error topping up balance', error: errorBody }), {
+	if (error) {
+		console.error('Error updating user balance:', error);
+		return new Response(JSON.stringify({ message: 'Error topping up balance', error }), {
 			status: 500
 		});
 	}
