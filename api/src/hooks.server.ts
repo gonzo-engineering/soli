e2cfec67c2daf1cfc3ee378b036dffc43c68d0cc
Frederick O'Brien <frederik.obrien@gmail.com>
2025-09-01 21:11:11 +0100

    Update hooks.server.ts

--- api/src/hooks.server.ts
+++ api/src/hooks.server.ts
@@ -6,6 +6,25 @@ const domainsWithAccessToAPI = [APP_DOMAIN, DASHBOARD_DOMAIN, 'https://checkout.
 
 export const handle: Handle = async ({ resolve, event }) => {
 	const origin = event.request.headers.get('origin');
+	const requestBody = await event.request.json();
+
+	const isStripeCheckout = requestBody.type !== 'checkout.session.completed';
+
+	const appendHeaders = (response: Response, origin: string | null) => {
+		response.headers.append(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+		);
+		response.headers.append('Access-Control-Allow-Headers', '*');
+		response.headers.append('Access-Control-Allow-Origin', origin ? origin : '*');
+		return response;
+	};
+
+	if (isStripeCheckout) {
+		const response = await resolve(event);
+		appendHeaders(response, origin);
+		return response;
+	}
 
 	if ((origin === null || !domainsWithAccessToAPI.includes(origin)) && !dev) {
 		console.error(`Unauthorized access attempt from origin: ${origin}`);
@@ -13,8 +32,6 @@ export const handle: Handle = async ({ resolve, event }) => {
 	}
 
 	const response = await resolve(event);
-	response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-	response.headers.append('Access-Control-Allow-Headers', '*');
-	response.headers.append('Access-Control-Allow-Origin', origin ? origin : '*');
+	appendHeaders(response, origin);
 	return response;
 };
