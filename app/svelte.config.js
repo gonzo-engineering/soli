05978d99413fa6e649f7f678e990f7bbbcdbf1f8
Frederick O'Brien <frederik.obrien@gmail.com>
2025-09-11 23:18:12 +0100

    App dependencies and formatting

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,7 @@ const config = {
 	kit: {
 		adapter: adapter()
 	},
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
