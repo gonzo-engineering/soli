2e0b10b6e5f5d5f92e5c8d7f1bb89c6ce601be02
Frederick O'Brien <frederik.obrien@gmail.com>
2025-09-11 23:21:51 +0100

    Payroll dependencies

--- payroll/scripts/payroll.ts
+++ payroll/scripts/payroll.ts
@@ -93,7 +93,7 @@ const artistEarningsWithNames = artistIds.map((artistId) => {
 
 // Send a payout to each artist
 export const stripe = new Stripe(STRIPE_KEY, {
-  apiVersion: "2025-06-30.basil",
+  apiVersion: "2025-08-27.basil",
 });
 
 const prettifyPennies = (pence: number) => {
