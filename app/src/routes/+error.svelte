0e64eda0e233de6ce4c4f9b2d955611217acef4f
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2025-10-27 02:01:09 +0000

    Basic error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="error-message-container">
+	<h2>{page.status}: {page.error?.message}</h2>
+
+	<p>Something went wrong. Let's <a href="/">take it from the top</a>.</p>
+</div>
+
+<style>
+	.error-message-container {
+		text-align: center;
+		margin: 5rem 0;
+	}
+</style>
