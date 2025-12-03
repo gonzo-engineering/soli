3b1162cb8547546db34958a3ccf2d950e4a0df89
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2025-12-03 18:13:29 +0000

    No index for certain roots, GoatCounter script for everything else

--- app/src/lib/global/config.ts
+++ app/src/lib/global/config.ts
@@ -4,7 +4,17 @@ import { API_DOMAIN, API_LOCAL_PORT, APP_DOMAIN, APP_LOCAL_PORT } from '../../..
 export const PUBLIC_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 export const PUBLIC_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const PUBLIC_PATH_ROOTS = ['/login', '/auth', '/about', '/contact', '/privacy', '/terms'];
+export const PUBLIC_PATH_ROOTS = ['/login', '/auth', '/about', '/contact', '/privacy', '/terms-of-service'];
+export const INDEXABLE_PATH_ROOTS = [
+		'/releases',
+		'/artists',
+		'/genres',
+		'/about',
+		'/terms-of-service',
+		'/privacy',
+		'/contact',
+		'/login',
+	];
 
 export const API_BASE = dev ? `http://localhost:${API_LOCAL_PORT}` : API_DOMAIN;
 const APP_BASE = dev ? `http://localhost:${APP_LOCAL_PORT}` : APP_DOMAIN;
