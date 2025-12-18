3a2a6805c61015dca4ff943466ec348431707eea
Frederick O'Brien <frederik.obrien@gmail.com>
2025-12-18 01:41:36 +0000

    Polish up behaviour

--- app/src/lib/components/audio-player/AudioPlayer.svelte
+++ app/src/lib/components/audio-player/AudioPlayer.svelte
@@ -7,6 +7,7 @@
 	import { STREAM_THRESHOLD_SECONDS } from '../../../../../shared/config';
 	import type { Track } from '../../../../../shared/types/core';
 	import type { ReleaseHydrated, TrackHydrated } from '../../../../../shared/types/hydrated';
+	import Chevron from '../icons/Chevron.svelte';
 	import ButtonWrapper from '../layout/ButtonWrapper.svelte';
 	import ReleaseArtwork from '../ReleaseArtwork.svelte';
 	import TrackLikeButton from '../releases/TrackLikeButton.svelte';
@@ -102,11 +103,11 @@
 <div
 	class="audio-player"
 	class:full-page={fullPage}
-	transition:slide={{ duration: 300, axis: 'x' }}
+	transition:slide={{ duration: 300, axis: 'y' }}
 >
 	{#if fullPage}
 		<img src="/full-logo-black.png" class="soli-logo" alt="Soli emblem" />
-		<h2>Now Playing</h2>
+		<h3>Now Playing</h3>
 		<ReleaseArtwork
 			name={release.title}
 			artist={release.artist.name}
@@ -114,45 +115,51 @@
 			isAskew
 		/>
 	{/if}
-	<div class="now-playing-info">
-		<div>
-			“{track.title}” by
-			<a href={`/artists/${release.artist_id}`} onclick={() => (fullPage = false)}
-				>{release.artist.name}</a
+	<div class="mobile-wrapper" class:reverse-column={fullPage}>
+		<div class="hidden-on-desktop">
+			<ButtonWrapper
+				onClickFunction={() => {
+					fullPage = !fullPage;
+				}}
 			>
+				<div style:transform={fullPage ? 'rotate(0deg)' : 'rotate(180deg)'}>
+					<Chevron />
+				</div>
+			</ButtonWrapper>
 		</div>
-		<TrackLikeButton trackID={track.id} {likedTracks} lightOrDark={'dark'} />
-	</div>
-	<audio
-		bind:this={audioElement}
-		src={songUrl}
-		bind:paused={userState.activeSongIsPaused}
-		ontimeupdate={onTimeUpdate}
-		onended={() => {
-			if (userState.autoPlay) {
-				const currentSongIndex = release.tracks.findIndex(
-					(track) => track.ipfs_cid === userState.activeSong?.ipfs_cid
-				);
-				if (currentSongIndex !== -1 && currentSongIndex < release.tracks.length - 1) {
-					const nextSong = release.tracks[currentSongIndex + 1];
-					setActiveSong(nextSong, release, userBalance, userPayPerStream);
-				} else {
-					userState.autoPlay = false;
-				}
-			}
-		}}
-		controls
-		autoplay
-		controlsList="nodownload noplaybackrate"
-	></audio>
-	<div class="hidden-on-desktop">
-		<ButtonWrapper
-			onClickFunction={() => {
-				fullPage = !fullPage;
-			}}
-		>
-			{fullPage ? 'Minimize' : 'Expand'}
-		</ButtonWrapper>
+		<div class="essentials">
+			<div class="now-playing-info">
+				<div>
+					“{track.title}” by
+					<a href={`/artists/${release.artist_id}`} onclick={() => (fullPage = false)}
+						>{release.artist.name}</a
+					>
+				</div>
+				<TrackLikeButton trackID={track.id} {likedTracks} lightOrDark={'dark'} />
+			</div>
+			<audio
+				bind:this={audioElement}
+				src={songUrl}
+				bind:paused={userState.activeSongIsPaused}
+				ontimeupdate={onTimeUpdate}
+				onended={() => {
+					if (userState.autoPlay) {
+						const currentSongIndex = release.tracks.findIndex(
+							(track) => track.ipfs_cid === userState.activeSong?.ipfs_cid
+						);
+						if (currentSongIndex !== -1 && currentSongIndex < release.tracks.length - 1) {
+							const nextSong = release.tracks[currentSongIndex + 1];
+							setActiveSong(nextSong, release, userBalance, userPayPerStream);
+						} else {
+							userState.autoPlay = false;
+						}
+					}
+				}}
+				controls
+				autoplay
+				controlsList="nodownload noplaybackrate"
+			></audio>
+		</div>
 	</div>
 </div>
 
@@ -171,7 +178,7 @@
 		background-color: #f0f0f0;
 		padding: 1rem;
 	}
-	h2 {
+	h3 {
 		margin: 0;
 	}
 	.full-page {
@@ -190,11 +197,24 @@
 		display: flex;
 		flex-direction: row;
 		align-items: center;
+		justify-content: center;
 		gap: 0.5rem;
 	}
+	.mobile-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		width: 100%;
+	}
+	.reverse-column {
+		flex-direction: column-reverse;
+	}
 	@media (min-width: 600px) {
-		.audio-player {
+		.essentials {
+			display: flex;
 			flex-direction: row;
+			text-align: center;
+			justify-content: space-evenly;
 			gap: 2rem;
 		}
 		.hidden-on-desktop {
