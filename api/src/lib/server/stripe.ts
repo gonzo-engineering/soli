103ebb63c5b4988fc0554d6c83f08ac3881b8374
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2026-05-29 23:16:18 +0100

    May 2026 dependencies bump

--- api/src/lib/server/stripe.ts
+++ api/src/lib/server/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from 'stripe';
 import { STRIPE_SECRET_KEY } from '$env/static/private';
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-	apiVersion: '2026-03-25.dahlia'
+	apiVersion: '2026-05-27.dahlia'
 });
